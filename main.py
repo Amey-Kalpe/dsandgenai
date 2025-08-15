12c7a144bbd1dca3bd60b2c67219a734be8afa04
Amey-Kalpe <ameykalpe16@gmail.com>
2025-08-16 00:02:50 +0530

    running the workflow with and without customer id

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+import uuid
+from langchain_core.messages import HumanMessage
+from langgraph.types import Command
+from graph import multi_agent_verify_graph
+
+
 def main():
-    print("Hello from ai-agents!")
+    # Without Customer ID
+    thread_id = uuid.uuid4()
+    question = "How much was my most recent purchase?"
+    config = {"configurable": {"thread_id": thread_id}}
+
+    result = multi_agent_verify_graph.invoke(
+        {"messages": [HumanMessage(content=question)]}, config=config
+    )
+    for message in result["messages"]:
+        message.pretty_print()
+
+    # Resume from the interrupt, providing the phone number for verification
+    question = "My phone number is +55 (12) 3923-555."
+    result = multi_agent_verify_graph.invoke(Command(resume=question), config=config)
+    for message in result["messages"]:
+        message.pretty_print()
+
+    print("*" * 50)
+    print("*" * 50)
+
+    # With customer ID
+    thread_id = uuid.uuid4()
+    question = "How much was my most recent purchase? My customer ID is 1"
+    config = {"configurable": {"thread_id": thread_id}}
+
+    result = multi_agent_verify_graph.invoke(
+        {"messages": [HumanMessage(content=question)]}, config=config
+    )
+    for message in result["messages"]:
+        message.pretty_print()
 
 
 if __name__ == "__main__":
